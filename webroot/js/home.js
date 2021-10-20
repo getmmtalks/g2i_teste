86222f6b2cd75d720dbca93abfdeb227c74e79f3
getmmtalks <89320418+getmmtalks@users.noreply.github.com>
2021-10-20 12:12:47 -0400

    Update home.js

--- webroot/js/home.js
+++ webroot/js/home.js
@@ -48,6 +48,8 @@ $(document).ready(function(){
             
             $('#buscar-veiculos-resultado').append(html);
             
+            $('#buscar-veiculos-resultado').css('display','block');
+            
         });
 
     });
